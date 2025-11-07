437b5bcec194f62715b515f26928b1b5dd9b4dd8
Erik <erik.siljestrom@iths.se>
2025-11-07 10:24:32 +0100

    Updated and added logic to cinema-program

--- Applications/CinemaDiscountApplication.cs
+++ Applications/CinemaDiscountApplication.cs
@@ -10,62 +10,71 @@ namespace Övning2.Applications
 	{
 		internal enum Prices { Youth = 80, Senior = 90, Standard = 120}
 		internal enum Ages { Youth = 20, Senior = 65 }
-		public CinemaDiscountApplication(IUI ui) : base(ui) { } // Inherits the _ui instance from the base class "ApplicationBase"
+		public CinemaDiscountApplication(IUI ui) : base(ui) { } // Ärver _ui instansen från basklassen "ApplicationBase"
+		public CinemaDiscountApplication(IUI ui, string choice) : base(ui)
+		{
+			// körs endast när man skapar ett object med en string (var tvungen att göra såhär eftersom IApplication endast har en Run metod
+			RunDiscountCheck();
+		}
 		public override void Run()
 		{
-			
+			uint quantity = _ui.PromptInput<uint>("Hur många är ni som ska gå på bio? ");
+			List<Ticket> tickets = new List<Ticket>();
+
+			tickets.Sum(t => t.Price);
+
+			for(int i = 1; i <= quantity; i++)
+			{
+				_ui.Print($"Person #{i}: ");
+				uint age = _ui.PromptInput<uint>("Ange ålder: ");
+
+				tickets.Add(new Ticket(age));
+			}
+
+			_ui.Print($"Antal: {tickets.Count} personer");
+			_ui.Print($"Kostnad: {tickets.Sum(t => t.Price)}kr");
 		}
 
 		public void RunDiscountCheck()
 		{
-			int age = InputAge();
+			uint age = _ui.PromptInput<uint>("Ange ålder: ");
+			string discount = CheckDiscount(age);
 
-			if (age > 0)
-			{
-				if (age < (int)Ages.Youth)
-					_ui.Print($"Ungdomspris: {Prices.Youth}kr");
-				if (age > (int)Ages.Senior)
-					_ui.Print($"Pensionärspris: {Prices.Senior}kr");
-				else
-					_ui.Print($"Standardpris: {Prices.Standard}kr");
-			}
-		}
-
-		private int InputAge()
-		{
-			_ui.Print("Ange ålder: ");
-			return _ui.Input<int>();
-		}
-
-		private void AddTicket()
-		{
-			var ticket = new Ticket("ha", 12);
-		}
-
-		private int GetPrice(int age)
-		{
-			int price;
-			if (age < (int)Ages.Youth)
-				price = (int)Prices.Youth;
-			if (age >= (int)Ages.Senior)
-				price = (int)Prices.Senior;
+			if (discount == "Youth")
+				_ui.Print($"Ungdomspris: {(int)Prices.Youth}kr");
+			else if (discount == "Senior")
+				_ui.Print($"Pensionärspris: {(int)Prices.Senior}kr");
 			else
-				price = (int)Prices.Standard;
+				_ui.Print($"Standardpris: {(int)Prices.Standard}kr");
+		}
 
-			return price;
+		private static string CheckDiscount(uint age)
+		{
+			if (age < (uint)Ages.Youth)
+				return "Youth";
+			if (age >= (uint)Ages.Senior)
+				return "Senior";
+			else
+				return "Standard";
 		}
 
 		private class Ticket
 		{
-			private string Name { get; set; }
-			private int Price { get; set; }
+			internal uint Price { get; set; } = (int)Prices.Standard;
+			private string Discount { get; set; } = "Standard";
 
-			public Ticket(string name, int age)
+			public Ticket(uint age)
 			{
-				Name = name;
-				
+				Discount = CheckDiscount(age);
+				if (Discount == "Youth")
+				{
+					Price = (uint)Prices.Youth;
+				}
+				else if (Discount == "Senior")
+				{
+					Price = (uint)Prices.Senior;
+				}
 			}
-
 		}
 
 	}
