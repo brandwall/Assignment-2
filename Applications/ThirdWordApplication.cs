d7826adc554ce5d7779fd292122752972c7d30d9
Erik <erik.siljestrom@iths.se>
2025-11-07 10:23:18 +0100

    Added logic for third word-game

--- Applications/ThirdWordApplication.cs
+++ Applications/ThirdWordApplication.cs
@@ -11,6 +11,23 @@ namespace Övning2.Applications
 		public ThirdWordApplication(IUI ui) : base(ui) { }
 		public override void Run()
 		{
+			while (true)
+			{
+				string input = _ui.PromptInput<string>("Ange en mening med minst tre ord: ");
+				var splitInput = input.Split(' ');
+
+				// Kontrollerar om man antingen har angivit fler mellanslag efter varann eller har angivit mindre än 3 ord
+				if (splitInput.Contains(string.Empty) || splitInput.Length < 3)
+				{
+					_ui.Clear();
+					_ui.Print("Försök igen! ");
+				}
+				else if (splitInput.Length >= 3)
+				{
+					_ui.Print(splitInput[2]);
+					break;
+				}
+			}
 
 		}
 	}
